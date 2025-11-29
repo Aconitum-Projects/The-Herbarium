a70bbe69ac6acd259b983ba6fc122e3bd0adfa3b
Giu <giulia.perucca@gmail.com>
2025-11-29 18:58:11 +0100

    Cleaning files

--- Herbarium — Unity Folder/Assets/Scripts/GameController.cs
+++ Herbarium — Unity Folder/Assets/Scripts/GameController.cs
@@ -23,10 +23,10 @@ public class GameController : MonoBehaviour
             characterMovements[i].SetSelectionEffect(false);
         }
         
-        GameTrigger gameTrigger = FindObjectsByType<GameTrigger>();
-        if (gameTrigger != null)
+        //GameTrigger gameTrigger = FindObjectsByType<GameTrigger>();
+        //if (gameTrigger != null)
         {
-            gameTrigger.PlayerPlayable();
+            //gameTrigger.PlayerPlayable();
         }
         
         UpdateCharacterControl();
